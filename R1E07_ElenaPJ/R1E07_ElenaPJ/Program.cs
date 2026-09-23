de77586ad0dfdedc09b583acd3a22072c719d4c3
Elena Pérez <elepeji2003@gmail.com>
2026-09-23 21:32:11 +0200

    Add files via upload

---
+++ R1E07_ElenaPJ/R1E07_ElenaPJ/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace R1E07_ElenaPJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONTABLES
+
+            // VARIABLES
+            string nombre;
+            string apellidos;
+            int edad;
+            // ENTRADA
+            Console.WriteLine("Introduce tu nombre ");
+            Console.Write("Nombre: " );
+            nombre = Console.ReadLine();
+
+            Console.Write("Introduce tus apellidos: ");
+            apellidos = Console.ReadLine();
+
+            Console.Write("Introduce tu edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Los datos introducidos son: ");
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Apellidos: " + apellidos);
+            Console.WriteLine("Edad: " + edad);
+
+            // PROCESO
+
+            // SALIDA
+        }
+    }
+}
