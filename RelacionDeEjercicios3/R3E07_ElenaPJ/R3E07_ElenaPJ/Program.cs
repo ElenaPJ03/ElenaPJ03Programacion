0ccd41a454acb47d5903ab75e8ff07735a78601a
Elena Pérez <elepeji2003@gmail.com>
2026-09-25 14:33:31 +0200

    A

---
+++ RelacionDeEjercicios3/R3E07_ElenaPJ/R3E07_ElenaPJ/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace R3E07_ElenaPJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONSTANTES
+
+            // VARIABLES
+            byte edad;
+            bool esMayorEdad;
+
+            string cadenaAuxiliar;
+
+            // ENTRADA
+            //Solicitar al usuario que introduzca su edad
+            Console.Write("Introduce tu edad: ");
+            cadenaAuxiliar = Console.ReadLine();
+            edad = Convert.ToByte(cadenaAuxiliar);
+
+            // PROCESO
+            //Calcular si el usuario es mayor de edad
+            esMayorEdad = edad >= 18;
+
+            // SALIDA
+            //Mostrar al usuario su edad y si es mayor de edad
+            Console.WriteLine("El usuario con " + edad + " años");
+            Console.Write("Mayor de edad: " + esMayorEdad);
+        }
+    }
+}
---
+++ RelacionDeEjercicios3/R3E08_ElenaPJ/R3E08_ElenaPJ/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace R3E08_ElenaPJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONSTANTES
+
+            // VARIABLES
+            int numero;
+            bool esPar;
+
+            string cadenaAuxiliar;
+
+            // ENTRADA
+            //Solicitar al usuario que introduzca un número
+            Console.Write("Introduce un número: ");
+            cadenaAuxiliar = Console.ReadLine();
+            numero = Convert.ToInt32(cadenaAuxiliar);
+
+            // PROCESO
+            //Comprobar si el número es par o impar
+            esPar = numero % 2 == 0;
+
+            // SALIDA
+            //Mostrar el número introducido y si es par o impar
+            Console.WriteLine($"El número {numero}");
+            Console.Write($"Es par: {esPar}");
+        }
+    }
+}
